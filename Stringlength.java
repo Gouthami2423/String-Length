b74cc8916040e10b43eac0185d1a09e3b9efd6bb
gouthami~ <kgowthami167@gmail.com>
2025-01-28 18:51:10 +0530

    Add files via upload

---
+++ Stringlength.java
@@ -0,0 +1,7 @@
+public class Stringlength {
+    public static void main(String[] args) {
+        String str = "hello,world";
+        int length=str.length();
+        System.out.println("the length of string is:" + length);
+    }
+}
